2dacf8f17bfd820c38cdb425ce52e7d285209283
FatChickn <ferdinandoaspro2@gmail.com>
2024-01-21 11:59:28 +0100

    added active states

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const form = document.querySelector("form");
 const input = document.querySelector("input");
+const error = document.querySelector(".error");
 const mail = document.querySelector(".mail");
 const content = document.querySelector(".content");
 const message = document.querySelector(".message");
@@ -7,11 +8,26 @@ const dismiss = document.querySelector(".message button");
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    content.classList.toggle("inactive");
-    message.classList.toggle("inactive");
-    mail.innerText = input.value;
+
+    if (input.value.trim().length === 0) {
+        input.required = true;
+        error.classList.toggle("inactive");
+    } else {
+        content.classList.toggle("inactive");
+        message.classList.toggle("inactive");
+        mail.innerText = input.value.trim();
     }
-);
+});
+
+input.addEventListener("input", () => {
+    if (input.checkValidity()) {
+        input.required = false;
+        error.classList.add("inactive");
+    } else {
+        input.required = true;
+        error.classList.remove("inactive");
+    }
+})
 
 dismiss.addEventListener("click", () => {
     content.classList.toggle("inactive");
